6afe615e7c94410684c8aa5692fadba4ff1f3606
Annisa Zalya Vianur Nasution <zalyaannisa@gmail.com>
2026-09-21 19:44:36 +0700

    app laravel sanctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(function (Request $request) {
+            return $request->is('api/*') ? null : '/';
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
